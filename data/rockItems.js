78f9d0605e3083ab824c3ec89e7d00b3c05cdcaf
Zachary Sawyer <mountainsounds15@gmail.com>
2021-11-25 12:53:44 -0500

    added more photos

--- data/rockItems.js
+++ data/rockItems.js
@@ -3,6 +3,17 @@ import dmm from '@/public/rock/dmm.jpeg';
 import metoliousCams from '@/public/rock/metoliousCams.jpeg';
 import sportivaShoes from '@/public/rock/sportivaShoes.jpeg';
 
+import bdCams from '@/public/rock/bdCams.jpeg';
+import eldelridCarabiner from '@/public/rock/eldelridCarabiner.jpeg';
+import grindstone from '@/public/rock/grindstone.jpeg';
+import grivelPlume from '@/public/rock/grivelPlume.jpeg';
+import metoliousCamMed from '@/public/rock/metoliousCamsMed.jpeg';
+import nutTool from '@/public/rock/nutTool.jpeg';
+import tibloc from '@/public/rock/tibloc.jpeg';
+import trangoKnife from '@/public/rock/trangoKnife.jpeg';
+import tricams from '@/public/rock/tricams.jpeg';
+import sportivaRock from '@/public/rock/sportivaRock.jpeg';
+
 const rockItems = [
     {
         image: atcGuide,
@@ -42,15 +53,95 @@ const rockItems = [
         retailPrice: '$60'
     },
     {
-        image: metoliousCams,
-        name: 'Metolious: Ultralight Master Cam',
-        info: 'Gray, size #00.',
-        condition: 'Great',
-        notes: 'Used around 10x or less.',
-        salePrice: '$30',
-        retailPrice: '$60'
+        image: bdCams,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
     },
-
+    {
+        image: eldelridCarabiner,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: grindstone,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: grivelPlume,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: metoliousCamMed,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: nutTool,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: tibloc,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: trangoKnife,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: tricams,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    },
+    {
+        image: sportivaRock,
+        name: '',
+        info: '',
+        condition: '',
+        notes: '',
+        salePrice: '',
+        retailPrice: ''
+    }
 ]
 
 export default rockItems;
