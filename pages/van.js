dc1286a6ac0d7232d0b914ea22ea06e2c1f3112e
Zack Sawyer <mountainsounds15@gmail.com>
2021-12-02 21:47:09 -0500

    van page styling improvements

--- pages/van.js
+++ pages/van.js
@@ -8,12 +8,13 @@ export default function Van() {
         <section id='vanPage' className="hero is-info">
             <Nav />
             <div className="hero-body">
-                <div className="container has-text-centered">
+                <div className="container">
                     <div id='vanInfo' className="">
-                        <h1 className="title"> 2019 Ford Transit Connect </h1>
+                        <h1 className="title has-text-centered"> 2019 Ford Transit Connect </h1>
+                        <h2 className="has-text-centered">Price: $27,000</h2>
                         <p>A much beloved converted cargo van with an eye to minimalism and function.
                             Smaller than a sprinter, it is a great size for everyday driving,
-                            as well as weekend and multi-week trips.</p>
+                            as well as weekend and multi-week trips. The price includes all the additional ammenties listed below. </p>
                         <p>It has spent all but the last few months in Southern California so rust free.
                             Please see the above window sticker for all the original specs.</p>
                         <br></br><br></br>
